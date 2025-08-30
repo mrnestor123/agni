349f8b7994f90217d3d76c2fc7411dcae9fa1508
mrnestor123 <mrnestor@MacBook-Air-de-Ernest.local>
2025-08-30 10:35:44 +0200

    cambios

--- theme.js
+++ theme.js
@@ -2,10 +2,11 @@
 
 /// SE CREA UN TEMA Y CON EL TEMA SE CREA LA PÁGINA
 let theme = {
-    maincolor:'#bb5141',
-
-    
-
+    maincolor:'#bb5141',    
+    grey: '#f1eff0',
+    green: '#b1bb9d', 
+    lightWhite: '#fdfbf7',
+    navbar: '#bb5141'
 }
 
 
